dc677fa45e6f46284bbd8791b41b11fe521b328a
Bhasin <bassanneeraj@gmail.com>
2026-06-08 09:59:33 +0530

    fruitBaskets correct using hashMap

--- fruitBaskets.java
+++ fruitBaskets.java
@@ -1,20 +1,22 @@
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 class fruitBaskets{
     public int maxFruits(int[] fruits){
-        Set<Integer> visited = new HashSet<>();
-        int baskets = 2 ;
-        int currBaskets = 0 ;
+        Map<Integer,Integer> baskets = new  HashMap<>();
+
         int maxFruites = 0 ;
         int i = 0 ;
         for(int j =0 ; j < fruits.length ; j++){
-            visited.add(fruits[j]);
-            currBaskets++;
-            while(currBaskets > baskets){
-                visited.remove(fruits[i]);
+            baskets.put(fruits[j], baskets.getOrDefault(fruits[j], 0)+1);
+            while(baskets.size()> 2){
+                baskets.put(fruits[i], baskets.getOrDefault(fruits[i],baskets.getOrDefault(fruits[i],0)-1));
+                if(baskets.get(fruits[i]) == 0){
+                    baskets.remove(fruits[i]);
+                }
                 i++;
-                currBaskets --;
             }
             maxFruites = Math.max(maxFruites, j-i+1);
         }
