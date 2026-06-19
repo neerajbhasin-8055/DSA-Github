77210329ec8887e0c44dc6120fc55b969a03f48e
Bhasin <bassanneeraj@gmail.com>
2026-06-19 09:57:21 +0530

    LC 1732

---
+++ Leetcode1732.java
@@ -0,0 +1,15 @@
+class Leetcode1732{
+  
+    public int largestAltitude(int[] gain) {
+        int[] result = new int[gain.length + 1];
+        int max = Integer.MIN_VALUE;
+        int i = 1;
+        for(int j = 0 ; j < gain.length ; j++){
+            result[i] = gain[j] + result[i-1];
+            max = Math.max(max,result[i]);
+            i++;
+        }
+        if(max < 0) return 0;
+        return max;
+    }
+}
