2188335841f8070a24fc18f629a13cad7ecb22f5
neerajbhasin-8055 <bassanneeraj@gmail.com>
2026-08-14 12:50:30 +0530

    Time: 13 ms (49.41%), Space: 44.4 MB (20.5%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -7,8 +7,17 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+ /* 
+ In this problem the main problem is we could have the nodes at the k distance in the upward direction of the target node
+ but in binary trees we can only explore in the one direction - so to solve that problem we will map every node to its parent.
+
+ 1 - use hashmap to map node with its parent - (BFS approach)
+ 2 - traverse from the target node, radially (upward, left and right) ;
+ 3 - calculate the distance at each and everypoint, once the distance == k the values that are there in the queue
+ will be the values we require as our result 
+ */
 class Solution {
-    // private Map<TreeNode,TreeNode> parentMap = new TreeMap<>();
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         List<Integer> result = new ArrayList<>();
         if(root == null) return result;
