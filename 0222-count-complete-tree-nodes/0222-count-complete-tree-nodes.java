80b88d1897dc8d1e45a3c1691e46c3b111290289
neerajbhasin-8055 <bassanneeraj@gmail.com>
2026-08-15 12:24:53 +0530

    Time: 0 ms (100%), Space: 49.3 MB (85.23%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -4,7 +4,7 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode() {}
+ *     TreeNode(
  *     TreeNode(int val) { this.val = val; }
  *     TreeNode(int val, TreeNode left, TreeNode right) {
  *         this.val = val;
@@ -13,17 +13,25 @@
  *     }
  * }
  */
+
+ /* 
+
+ Here we are given with a complete binary tree tha and its property is 
+ Total nodes = 2^h-1 (h = height of the binary tree)
+ and if we are standing at some particular node from where the left and right height is equal that means the tree is completely filled so we can simply use the formula and calculate the number of nodes for that level "( 2 << leftHeight/rightHeight) - 1"
+ and if the height is not equal we recursively visit each node and apply the above formula.
+ */
 class Solution {
     public int countNodes(TreeNode root) {
         if(root == null) return 0;
 
-        int lh = getLeftHeight(root);
-        int rh = getRightHeight(root);
+        int lh = getLeftHeight(root); // calculating left height
+        int rh = getRightHeight(root); // calculating right height
 
         if(lh == rh){
-            return (2 << lh) -1 ;
+            return (2 << lh) -1 ; // if equal apply formula
         }
-        return 1 + countNodes(root.left) + countNodes(root.right);
+        return 1 + countNodes(root.left) + countNodes(root.right); // recursive calls
     }
     public int getLeftHeight(TreeNode root){
         int cnt = 0 ;
